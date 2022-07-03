62e8ac312f2e5c18bcf7b1fcbbe4d4d1fe8d8de3
Andrew <andrewsmith2509@gmail.com>
2022-07-03 12:47:03 -0500

    Initial Commit

---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by Andrew Smith on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
