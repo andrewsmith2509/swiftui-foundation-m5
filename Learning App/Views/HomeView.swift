a323ab895abaf6b8f0ecf5367b9a35ca740ca6f7
Andrew <andrewsmith2509@gmail.com>
2022-07-04 13:28:41 -0500

    - add HomeViewRow - create HomeView

--- Learning App/Views/HomeView.swift
+++ Learning App/Views/HomeView.swift
@@ -12,13 +12,37 @@ struct HomeView: View {
     @EnvironmentObject var model:ContentModel
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            VStack(alignment: .leading) {
+                
+                Text("What do you want to do today?")
+                    .padding(.leading, 20)
+                
+                ScrollView {
+                    LazyVStack(spacing: 15) {
+                        ForEach(model.modules) { module in
+                            // Learning Card
+                            HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, lessons: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                            
+                            // Test Card
+                            HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, lessons: "\(module.test.questions.count) Questions", time: module.test.time)
+                            
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Get Started")
+            
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
     }
 }
---
+++ Learning App/Views/HomeViewRow.swift
@@ -0,0 +1,75 @@
+//
+//  HomeViewRow.swift
+//  Learning App
+//
+//  Created by Andrew Smith on 7/4/22.
+//
+
+import SwiftUI
+
+struct HomeViewRow: View {
+   
+    @EnvironmentObject var model: ContentModel
+    @State var image:String
+    @State var title:String
+    @State var description:String
+    @State var lessons:String
+    @State var time:String
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+                
+            
+            HStack(spacing: 40) {
+                Image(image)
+                    .resizable()
+                    .frame(width: 116, height: 116)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    
+                    Text(title)
+                        .bold()
+                    
+                    Text(description)
+                        .padding(.bottom, 20)
+                        .font(.caption)
+                    
+                    HStack{
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        
+                        Text(lessons)
+                            .font(Font.system(size: 10))
+                        
+                        Spacer()
+                        
+                        Image(systemName: "clock")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        
+                        Text(time)
+                            .font(Font.system(size: 10))
+
+                    }
+                }
+                    
+                    
+                    
+            }
+            .padding(.horizontal, 20)
+        }
+    }
+}
+
+struct HomeViewRow_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewRow(image: "swift", title: "learn Swift", description: "Information", lessons: "20 Lessons", time: "3 Hours")
+            .environmentObject(ContentModel())
+    }
+}
